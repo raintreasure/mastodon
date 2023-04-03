2ae30747c701d563f3ae82dcc3c4cc0fe8ad5987
Liangyu-Zhou <17816872833@163.com>
2023-04-03 15:14:16 +0000

    adjust phrasing

--- app/javascript/mastodon/features/ui/components/sign_in_banner.jsx
+++ app/javascript/mastodon/features/ui/components/sign_in_banner.jsx
@@ -32,7 +32,7 @@ const SignInBanner = () => {
     <div className='sign-in-banner'>
       <p><FormattedMessage id='sign_in_banner.text' defaultMessage='Sign in to follow profiles or hashtags, favourite, share and reply to posts. You can also interact from your account on a different server.' /></p>
 
-      <a href='/auth/sign_in' className='button button--block'><FormattedMessage id='sign_in_banner.web3auth_login' defaultMessage='Web3auth Sign in' /></a>
+      <a href='/auth/sign_in' className='button button--block'><FormattedMessage id='sign_in_banner.web3auth_login' defaultMessage='Web3Auth Sign in' /></a>
       <a href='/auth/sign_in' className='button button--block button-tertiary'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Email Sign in' /></a>
       {signupButton}
     </div>
