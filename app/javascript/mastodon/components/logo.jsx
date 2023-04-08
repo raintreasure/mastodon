dd8c5f3bb33fd522e9cd64439e84164937d0db29
Burson <burson@live.cn>
2023-04-08 15:11:08 +0800

    switch logo-theme (with bug)

--- app/javascript/mastodon/components/logo.jsx
+++ app/javascript/mastodon/components/logo.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Logo = (props) => (
-  <img className={'logo'} src='chinese-dark.png' alt='Chinese.org' style={{ maxWidth:'100%', height:props.h }} />
-);
+const Logo = (props) =>{
+  const logo_path = (document.body && document.body.classList.contains('theme-mastodon-light')) ? 'chinese-dark.png' : 'chinese-icon-s.png';
+  return (
+    <img className={'logo'} src={logo_path} alt='Chinese.org' style={{ maxWidth:'100%', height:props.h }} />
+  );
+};
 
 Logo.propTypes = {
   h: PropTypes.string.isRequired,
