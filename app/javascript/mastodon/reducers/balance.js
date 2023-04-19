aafe5f9cc5366ca9f719336359ca8d757ce255d2
raintreasure <17816872833@163.com>
2023-04-19 15:16:12 +0800

    fix double toast, and unbookmark

--- app/javascript/mastodon/reducers/balance.js
+++ app/javascript/mastodon/reducers/balance.js
@@ -10,8 +10,8 @@ export default function balance(state = initialState, action) {
   switch (action.type) {
   case UPDATE_BALANCE:
     return state.withMutations((state) => {
-      state.setIn(['new_balance'], action.new_balance);
-      state.setIn(['balance_increment'], action.balance_increment);
+      state.setIn(['new_balance'], { new_balance: action.new_balance, balance_increment:action.balance_increment });
+      // state.setIn(['balance_increment'], action.balance_increment);
     });
   case BALANCE_UPDATE_FAIL:
     return state;
