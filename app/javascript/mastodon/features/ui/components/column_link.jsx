0a0fce4670242ea7c804ca6a6451abf2309fb4e4
raintreasure <17816872833@163.com>
2023-05-06 08:35:03 +0800

    token page, token tabs

--- app/javascript/mastodon/features/ui/components/column_link.jsx
+++ app/javascript/mastodon/features/ui/components/column_link.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import Icon from 'mastodon/components/icon';
 import classNames from 'classnames';
 
-const ColumnLink = ({ icon, text, to, href, method, badge, transparent, id, ref, ...other }) => {
+const ColumnLink = ({ icon, text, to, href, method, badge, transparent, id, ...other }) => {
   const className = classNames('column-link', { 'column-link--transparent': transparent });
   const badgeElement = typeof badge !== 'undefined' ? <span className='column-link__badge'>{badge}</span> : null;
   const iconElement = typeof icon === 'string' ? <Icon id={icon} fixedWidth className='column-link__icon' /> : icon;
@@ -37,7 +37,6 @@ ColumnLink.propTypes = {
   badge: PropTypes.node,
   transparent: PropTypes.bool,
   id: PropTypes.string,
-  ref: PropTypes.object,
 };
 
 export default ColumnLink;
