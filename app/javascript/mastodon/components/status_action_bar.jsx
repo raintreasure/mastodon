24d0bf57f163a57a6782231261edb27ea419edc7
raintreasure <17816872833@163.com>
2023-05-31 18:14:37 +0800

    specify error message, handle insufficient $CHINESE and insufficient $MATIC situation

--- app/javascript/mastodon/components/status_action_bar.jsx
+++ app/javascript/mastodon/components/status_action_bar.jsx
@@ -11,7 +11,7 @@ import classNames from 'classnames';
 import { PERMISSION_MANAGE_USERS, PERMISSION_MANAGE_FEDERATION } from 'mastodon/permissions';
 import { transferModal } from '../actions/tokens';
 
-const web2LoggedInMessage = 'It seems that you logged in by web2, only web3 logged user can transfer $CHINESE. You can switch account after ';
+const noAddrMessage = 'wallet address has not loaded, please try again or refresh the page';
 const toAccountNoAddress = 'The account you transferred to has no wallet address, you may remind the account owner to set wallet address';
 
 const messages = defineMessages({
@@ -55,7 +55,7 @@ const messages = defineMessages({
   transferTitle: { id: 'account.transfer.title', defaultMessage: 'Transfer $CHINESE' },
   transferText: { id: 'account.transfer.text', defaultMessage: 'You are transferring $CHINESE to ' },
   transferConfirm: { id: 'account.transfer.confirm', defaultMessage: 'Transfer' },
-  transferWeb2LoggedIn: { id: 'account.transfer.web2_logged_in', defaultMessage: web2LoggedInMessage },
+  transferWeb2LoggedIn: { id: 'account.transfer.web2_logged_in', defaultMessage: noAddrMessage },
   transferWeb2Logout: { id: 'account.transfer.web2_logout', defaultMessage: 'Log out' },
   transferEmptyConfirm: { id: 'account.transfer.empty_confirm', defaultMessage: 'Confirm' },
   transferToAccountNoAddress: { id: 'account.transfer.to_account_no_address', defaultMessage: toAccountNoAddress },
