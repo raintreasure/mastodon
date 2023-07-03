77bd6798e361f9d1bac1a53abbdeffd0f83cc628
raintreasure <17816872833@163.com>
2023-07-03 16:54:22 +0800

    supplement merge missings

--- db/schema.rb
+++ db/schema.rb
@@ -196,9 +196,9 @@ ActiveRecord::Schema.define(version: 2023_06_05_085711) do
     t.datetime "requested_review_at"
     t.string "eth_address"
     t.decimal "balance"
-    t.boolean "given_fsn"
     t.boolean "given_native_token"
     t.decimal "subscription_fee"
+    t.boolean "given_fsn"
     t.index "(((setweight(to_tsvector('simple'::regconfig, (display_name)::text), 'A'::\"char\") || setweight(to_tsvector('simple'::regconfig, (username)::text), 'B'::\"char\")) || setweight(to_tsvector('simple'::regconfig, (COALESCE(domain, ''::character varying))::text), 'C'::\"char\")))", name: "search_index", using: :gin
     t.index "lower((username)::text), COALESCE(lower((domain)::text), ''::text)", name: "index_accounts_on_username_and_domain_lower", unique: true
     t.index ["domain", "id"], name: "index_accounts_on_domain_and_id"
